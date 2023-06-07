704761d3e56c35b3571f801ec5c31935f2241922
MUSTAFA <MUSTAFA@DESKTOP-ICRQ64U>
2023-06-08 02:00:50 +0500

    add t new class named Invoice Items

---
+++ OOADHotelManagementSystem/Models/InvoiceItems.cs
@@ -0,0 +1,10 @@
+﻿namespace OOADHotelManagementSystem.Models
+{
+    public class InvoiceItems
+    {
+        public int Room_rates { get; set; }
+        public int Plan_rates { get; set; }
+        public int DaysOfStay { get; set; }
+        public int Misccharges { get; set; }
+    }
+}

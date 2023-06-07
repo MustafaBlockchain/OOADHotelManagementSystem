70962db46ddf699a4295c7213f2637d8c651d15d
MUSTAFA <MUSTAFA@DESKTOP-ICRQ64U>
2023-06-07 23:08:24 +0500

    New Class

---
+++ OOADHotelManagementSystem/Models/Admin.cs
@@ -0,0 +1,10 @@
+﻿namespace OOADHotelManagementSystem.Models
+{
+    public class Admin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Hotel_Name { get; set; }
+        public long Hotel_ID { get; set; }
+    }
+}

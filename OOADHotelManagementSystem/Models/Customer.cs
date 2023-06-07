589cd2391a23af5d0af074a25b6290c5f4629943
MUSTAFA <MUSTAFA@DESKTOP-ICRQ64U>
2023-06-07 23:17:20 +0500

    Added more classes to model

---
+++ OOADHotelManagementSystem/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace OOADHotelManagementSystem.Models
+{
+    public class Customer
+    {
+        public int Customer_ID { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Phone_number { get; set; }
+        public string City { get; set; }
+        public string Country_state { get; set; }
+        public string Zip_code { get; set; }
+        public long Hotel_ID { get; set; }
+    }
+}
---
+++ OOADHotelManagementSystem/Models/Hotel.cs
@@ -0,0 +1,11 @@
+﻿namespace OOADHotelManagementSystem.Models
+{
+    public class Hotel
+    {
+        public long Hotel_ID { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
+        public string Country_state { get; set; }
+        public string Phone { get; set; }
+    }
+}
